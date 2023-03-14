690f12ddf9b73f6f6d94376eacee011d46cf4d95
HidraForce <vierabruno4@gmail.com>
2023-03-14 09:27:36 -0300

    Penultimo commit

--- Lista1_EX6.c
+++ Lista1_EX6.c
@@ -2,29 +2,39 @@
 
 int main()
 {
-    int i=0,num[10],fat[10],j=0,k=0,p=0;
-    for ( i = 0; i < 10; i++)
-    {
-       printf("Insira um numero\n-->");
-       scanf("%d",&num[i]);
-    }
-
-   for ( j = 0; j < 10; j++)
+   /**
+    * Var declaration
+    */
+   int i = 0, num[10], fat[10], j = 0, k = 0, p = 0;
+   int fatorial;
+   
+   /**
+    * Input loop
+    */
+   for (i = 0; i < 10; i++)
    {
-      for (k = 0; k < num[j]; k++)
+      printf("Insira um numero\n-->");
+      scanf("%d", &num[i]);
+   }
+
+   /**
+    * Calculation loop
+    */
+   for (j = 0; j < 10; j++)
+   {
+      fatorial = 1;
+      for (k = 1; k <= num[j]; k++)
       {
-         num[j] = num[j] * k;
+         fatorial = fatorial * k;
       }
-      
+      fat[j] = fatorial;
    }
-   
-   for ( p = 0; p < 10; p++)
-   {
-      int count = p+1;
-      printf("O fatorial do %d° é: %d\n",count, num[p]);
-   }
-   
 
-    
-    
+   /**
+    * Output loop
+    */
+   for (p = 0; p < 10; p++)
+   {
+      printf("O fatorial do é: %d\n", fat[p]);
+   }
 }
--- Lista1_EX9.c
+++ Lista1_EX9.c
@@ -2,49 +2,45 @@
 
 int main()
 {
-    int age[10],weight[10],i=0;
-    
-    for ( i = 0; i < 10; i++)
+  //Var declaration
+  int age[10], weight[10], i = 0;
+
+  //Input loop
+  for (i = 0; i < 10; i++)
+  {
+    printf("Inisra a idade da %d° pessoa\n-->", i + 1);
+    scanf("%d", &age[i]);
+    printf("Inisra o peso da %d° pessoa\n-->", i + 1);
+    scanf("%d", &weight[i]);
+  }
+  //Var declaration to start try of elements in array
+  int j = 0, k = 0, total_idade = 0, total_old = 0, count_old = 0, count_fat = 0;
+  int peso = weight[0];
+
+  //Loop for testing elements in array
+  for (j = 0; j < 10; j++)
+  {
+    total_idade = total_idade + age[j];
+    if (weight[j] > peso)
     {
-        printf("Inisra a idade da %d° pessoa\n-->",i+1);
-        scanf("%d",&age[i]);
-        printf("Inisra o peso da %d° pessoa\n-->",i+1);
-        scanf("%d",&weight[i]);
-
-        
+      peso = weight[j];
     }
-    int j=0,k=0,total_idade,total_old,count_old = 0,count_fat= 0;
-    int peso  = weight[0]; 
-    for (j = 0; j < 10; j++)
+
+    if (age[j] > 30 && age[j] < 40)
     {
-      total_idade = total_idade +age[j];
-      if(weight[j]>peso){
-        peso =weight[j];
-      }
-
-      /* if (age[j]>30 && age[j]<40)
-      {
-        total_old = total_old +age[j];
-        count_old++;
-
-      } */
-      weight[j] >70 ?count_fat++:0;
-      
-      
-        
+      total_old = total_old + age[j];
+      count_old = count_old + 1;
     }
-    float media = total_idade/10;
-   /*  float media_old = total_old/count_old;
- */
-    printf("A média das idades é de: %f\n",media);
-    printf("O maior peso é de: %d\n",peso);
-   /*  printf("A média das pessoas acima de 30 e com menos de 40 é de: %f\n",media_old); */
-    printf("A quantidade de pessoas acima de 70kg é de: %d\n",count_fat);
-    
-    
+    weight[j] > 70 ? count_fat++ : 0;
+  }
+
+  //Final calculations
+  float media = total_idade / 10;
+  float media_old = total_old / count_old;
+
+  //Output
+  printf("A média das idades é de: %f\n", media);
+  printf("O maior peso é de: %d\n", peso);
+  printf("A média das pessoas acima de 30 e com menos de 40 é de: %f\n", media_old);
+  printf("A quantidade de pessoas acima de 70kg é de: %d\n", count_fat);
 }
-/**
- * ERRO ATUAL ESTA NO CONTADOR DE PESSOAAS VELHAS
- * 
- * 
-*/
