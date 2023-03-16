15179855ce14beafd895d459e77f70dbc80e1de5
HidraForce <vierabruno4@gmail.com>
2023-03-16 10:48:36 -0300

    Review code 2

--- Lista1_EX5.c
+++ Lista1_EX5.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
+int teste_primo(int x)
+{
+    int i, resto = 0;
+    for (i = 1; i <= x; i++)
+    {
+        if (x % i == 0)
+        {
+            resto++;
+        }
+    }
+    if (resto == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
 
 int main()
 {
@@ -12,7 +31,7 @@ int main()
     int j = 0, count = 0, count_impar = 0, count_par = 0, sumPar = 0;
     for (j = 0; j < 10; j++)
     {
-        if (num[j] % num[j] == 0)
+        if (teste_primo(num[j])==1)
         {
             count = count + 1;
         }
@@ -30,8 +49,8 @@ int main()
     if (sumPar != 0)
     {
         int media = sumPar / count_par;
-        printf("A média dos numeros pares inseridos foi de: %d", media);
+        printf("A média dos numeros pares inseridos foi de: %d\n", media);
     }
-    printf("Foram inseridos %d Primos", count);
-    printf("Foram inseridos %d Impares", count_impar);
+    printf("Foram inseridos %d Primos\n", count);
+    printf("Foram inseridos %d Impares\n", count_impar);
 }
